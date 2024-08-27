e8e18b0fc8ce85605d5baaa4b0103a6f40108ab3
Timothy B <74752830+TIMOTHY1498@users.noreply.github.com>
2024-08-27 20:27:08 +0700

    Main Source Code in C
    hehe, this maybe harder 2 decompile

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <array>
+
+// Variables
+char inputsoal[10];
+
+// mtkipa-cheat01-G8
+
+char rumus1[1028] = "c = a + b";
+char rumus2[1028] = "a = vf - vi / t";
+char rumus2[1028] = "d = v / t";
+char rumus3[1028] = "sigmaT = 0";
+
+// additional addon
+
+c
